05f4e7fa99009481e965e74e38e3fe1ebcefb6ee
DaPiankov <DaPiankov@croc.ru>
2024-02-16 16:49:17 +0300

    [feat]: Вернул обычный Spring-web вместо Spring Reactive

---
+++ security/service/AuthenticationManager.java
@@ -0,0 +1,12 @@
+package security.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+/**
+ * Менеджер аутентификации.
+ */
+@Component
+@RequiredArgsConstructor
+public class AuthenticationManager {
+}
